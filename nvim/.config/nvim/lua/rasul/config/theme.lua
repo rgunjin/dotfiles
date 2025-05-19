45dbe02dfb85eb76d9cac20fa23261025c83e188
rgunjin <boyfromburn@gmail.com>
2025-05-19 14:36:48 +0200

    add background floatwindow neovim

--- nvim/.config/nvim/lua/rasul/config/theme.lua
+++ nvim/.config/nvim/lua/rasul/config/theme.lua
@@ -4,8 +4,19 @@ require("rasul.config.rosepine")
 local function ColorMyPencils()
   -- цветовая схема уже активирована rose-pine внутри rosepine.lua
   vim.api.nvim_set_hl(0, "Normal",      { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
+
+local opaque_bg = vim.fn.exists("g:colors_name") == 1
+                 and vim.fn.synIDattr(vim.fn.hlID("Normal"), "bg")
+                 or "#1b1d23"
+
+vim.api.nvim_set_hl(0, "MasonNormal", { bg = opaque_bg })
+vim.api.nvim_set_hl(0, "LazyNormal",  { bg = opaque_bg })
+
+vim.api.nvim_set_hl(0, "MasonBorder", { bg = opaque_bg })
+vim.api.nvim_set_hl(0, "LazyBorder",  { bg = opaque_bg })
 
 ColorMyPencils()
 
