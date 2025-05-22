1082af2e054ab3f4fc7fd44f1614e18b00456a0b
rgunjin <boyfromburn@gmail.com>
2025-05-22 16:09:04 +0200

    add info-skript tmux-cht.sh, change tmux binding

---
+++ tmux/.config/tmux/tmux-cht.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env fish
+
+# Задаём список языков и утилит
+set languages (string split " " "c cpp python rust golang")
+set core_utils (string split " " "find xargs sed awk")
+
+# Объединяем два списка
+set options $languages $core_utils
+
+# Выбираем через fzf
+set selected (printf "%s\n" $options | fzf)
+
+# Просим ввести запрос
+read -P "GIMMIE YOUR QUERY: " query
+
+set query (string trim $query)
+set query (string replace " " "+" $query)
+
+# Определяем что выбранно, вызывем соответсвующий cht.sh
+if test -n $selected
+    if contains $selected $languages
+        tmux neww "curl cht.sh/$selected/$query | less"
+    else
+        tmux neww "curl cht.sh/$selected~$query | less"
+    end
+else
+    exit 0
+end
