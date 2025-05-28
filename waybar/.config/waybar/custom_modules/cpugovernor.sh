1e04022ae5cfa9d1536eea5440917ce56bfbdce8
rgunjin <boyfromburn@gmail.com>
2025-05-28 22:13:16 +0200

    remove custom cpu_goverment modul

--- waybar/.config/waybar/custom_modules/cpugovernor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-gov=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
-
-if [[ "$gov" == "performance" ]]; then
-	echo '{"text": "perf", "alt": "perf", "class": "performance", "tooltip": "<b>Governor</b> Performance"}'
-elif [[ "$gov" == "schedutil" ]]; then
-	echo '{"text": "sched", "class": "schedutil", "tooltip": "<b>Governor</b> Schedutil"}'
-fi
