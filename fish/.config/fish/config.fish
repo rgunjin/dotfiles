81b93ceadee1f15d3355789de67657cd9629aeb7
rgunjin <boyfromburn@gmail.com>
2025-06-01 18:24:25 +0200

    remove xcb and x11 from config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,8 +3,8 @@ if status is-interactive
     set -gx XDG_SESSION_TYPE wayland
     set -gx XDG_CURRENT_DESKTOP sway
     set -gx XDG_SESSION_DESKTOP sway
-    set -gx GDK_BACKEND wayland,x11
-    set -gx QT_QPA_PLATFORM wayland;xcb
+    set -gx GDK_BACKEND wayland
+    set -gx QT_QPA_PLATFORM wayland
     set -gx MOZ_ENABLE_WAYLAND 1
     set -gx SDL_VIDEODRIVER wayland
 
