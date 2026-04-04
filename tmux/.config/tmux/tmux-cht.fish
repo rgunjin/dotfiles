e7e55403f28a1f877b85c12cc7e18bdb4dd3d328
rgunjin <boyfromburn@gmail.com>
2026-04-04 19:24:49 +0200

    debugs

--- tmux/.config/tmux/tmux-cht.fish
+++ tmux/.config/tmux/tmux-cht.fish
@@ -1,28 +1,22 @@
 #!/usr/bin/env fish
 
-# Задаём список языков и утилит
-set languages (string split " " "c cpp python rust golang")
-set core_utils (string split " " "cat read grep echo tr find xargs sed awk")
+set languages c cpp python rust golang
+set core_utils cat read grep echo tr find xargs sed awk
 
-# Объединяем два списка
 set options $languages $core_utils
 
-# Выбираем через fzf
 set selected (printf "%s\n" $options | fzf)
 
-# Просим ввести запрос
-read -P "GIMMIE YOUR QUERY: " query
-
-set query (string trim $query)
-set query (string replace " " "+" $query)
-
-# Определяем что выбранно, вызывем соответсвующий cht.sh
-if test -n $selected
-    if contains $selected $languages
-        tmux neww "curl cht.sh/$selected/$query | less"
-    else
-        tmux neww "curl cht.sh/$selected~$query | less"
-    end
-else
+if test -z "$selected"
     exit 0
+end
+
+read -P "GIMMIE YOUR QUERY: " query
+set query (string trim $query)
+set query (string replace -a " " "+" $query)
+
+if contains -- $selected $languages
+    tmux neww "curl cht.sh/$selected/$query | less"
+else
+    tmux neww "curl cht.sh/$selected~$query | less"
 end
