988e61851d6143c0658cd1d6a53258a39196a90d
rgunjin <boyfromburn@gmail.com>
2026-05-31 20:42:45 +0200

    debug: cursor beam style come back

--- nvim/.config/nvim/lua/rasul/config/autocmds.lua
+++ nvim/.config/nvim/lua/rasul/config/autocmds.lua
@@ -1,17 +1,8 @@
 vim.o.guicursor = "n-v-c:block,i-ci:ver25,r-cr:hor20,o:hor50"
 
-local function restore_terminal_cursor()
-  vim.cmd("set guicursor=a:ver20,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor")
-end
-
-vim.api.nvim_create_autocmd({ "VimLeavePre", "VimSuspend" }, {
-  callback = restore_terminal_cursor,
-})
-
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "*",
   callback = function()
     vim.opt_local.formatoptions:remove({ "c", "r", "o" })
   end,
 })
-
