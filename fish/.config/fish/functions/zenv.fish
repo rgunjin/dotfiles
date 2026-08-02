6cd95e80763dacdd341f450770a09be698ca500a
rgunjin <boyfromburn@gmail.com>
2026-08-02 19:08:17 +0200

    debug: add flag -gx to Variables

--- fish/.config/fish/functions/zenv.fish
+++ fish/.config/fish/functions/zenv.fish
@@ -1,7 +1,7 @@
 function zenv --description 'Zephyr dev environment'
     source ~/zephyrproject/.venv/bin/activate.fish
-    set -x ZEPHYR_BASE ~/zephyrproject/zephyr
-    set -x ZEPHYR_TOOLCHAIN_VARIANT zephyr
-    set -x ZEPHYR_SDK_INSTALL_DIR /opt/zephyr-sdk-1.0.1
+    set -gx ZEPHYR_BASE ~/zephyrproject/zephyr
+    set -gx ZEPHYR_TOOLCHAIN_VARIANT zephyr
+    set -gx ZEPHYR_SDK_INSTALL_DIR /opt/zephyr-sdk-1.0.1
     echo "zephyr "(git -C $ZEPHYR_BASE describe --tags)" | sdk 1.0.1"
 end
