64ade64da7ec21e7608e6d8460953dbac5686e90
rgunjin <boyfromburn@gmail.com>
2026-08-02 19:02:43 +0200

    add Zephyr dev enviroment function

--- fish/.config/fish/functions/log.fish
+++ fish/.config/fish/functions/log.fish
@@ -2,6 +2,6 @@ function log --description "Append an entry to today's log"
     set -l dir ~/notes/logs
     set -l file $dir/(date +%F).md
     mkdir -p $dir
-    test -e $file; or printf '# %s\n\n' (date +%F) > $file
-    printf -- '- %s %s\n' (date +%H:%M) "$argv" >> $file
+    test -e $file; or echo "# "(date +%F) > $file
+    echo "- "(date +%H:%M)" $argv" >> $file
 end
